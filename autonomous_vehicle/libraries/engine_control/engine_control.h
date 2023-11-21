b718f6720e0bdc8fae11bab3cccbc5f212196d3d
Spaceship <76214766+webview7@users.noreply.github.com>
2023-11-21 15:06:59 +0100

    Update engine_control.h

--- autonomous_vehicle/libraries/engine_control/engine_control.h
+++ autonomous_vehicle/libraries/engine_control/engine_control.h
@@ -1,14 +1,5 @@
 #include <engine_control_component.h>
 
-int engine1 = 10;
-int engine2 = 5;
-
-int input_pin1 = 9;
-int input_pin2 = 8;
-
-int input_pin3 = 7;
-int input_pin4 = 6;
-
 void engine_control(int pin, int engine_id, int mode, int speed) {
   digitalWrite(pin, mode);
   if(speed == "") {
