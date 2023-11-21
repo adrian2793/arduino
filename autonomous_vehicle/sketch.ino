20af146f5c20d84935c87bd7ac11e243f3e2b7a4
Spaceship <76214766+webview7@users.noreply.github.com>
2023-11-21 14:48:01 +0100

    Update sketch.ino

--- autonomous_vehicle/sketch.ino
+++ autonomous_vehicle/sketch.ino
@@ -9,7 +9,10 @@ int input_pin4 = 6;
 
 void engine_control(int pin, int engine_id, int mode, int speed) {
   digitalWrite(pin, mode);
-  analogWrite(engine_id, speed);
+  if(speed == "") {
+  } else {
+    analogWrite(engine_id, speed);
+  }
 }
 
 void setup() {
@@ -22,21 +25,6 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(input_pin1, HIGH);
-  digitalWrite(input_pin2, LOW);
-  analogWrite(engine1, 200); 
-  digitalWrite(input_pin3, HIGH);
-  digitalWrite(input_pin4, LOW);
-  analogWrite(engine2, 200);
-  delay(2000);
-  digitalWrite(input_pin1, LOW);
-  digitalWrite(input_pin2, HIGH);  
-  digitalWrite(input_pin3, LOW);
-  digitalWrite(input_pin4, HIGH); 
-  delay(2000);
-  digitalWrite(input_pin1, LOW);
-  digitalWrite(input_pin2, LOW);  
-  digitalWrite(input_pin3, LOW);
-  digitalWrite(input_pin4, LOW);
-  delay(2000);
+  engine_control(9, 10, 1, 200);
+  engine_control(8, 10, 0, 0);
 }
