8e83f24441edf06e41d9175f685e7fad1bb86509
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-28 12:16:05 +0100

    Update engine_control.h

--- autonomous_vehicle/libraries/engine_control/engine_control.h
+++ autonomous_vehicle/libraries/engine_control/engine_control.h
@@ -7,17 +7,3 @@ void engine_control(int pin, int engine_id, int mode, int speed) {
     analogWrite(engine_id, speed);
   }
 }
-
-void setup() {
-  pinMode(engine1, OUTPUT);    
-  pinMode(engine2, OUTPUT);
-  pinMode(input_pin1, OUTPUT);
-  pinMode(input_pin2, OUTPUT);
-  pinMode(input_pin3, OUTPUT);
-  pinMode(input_pin4, OUTPUT);
-}
-
-void loop() {
-  engine_control(9, 10, 1, 200);
-  engine_control(8, 10, 0, 0);
-}
