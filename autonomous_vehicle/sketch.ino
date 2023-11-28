4c7373a4eef76a094f5f6beb3af6d6db9a6f686a
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-28 12:16:22 +0100

    Update sketch.ino

--- autonomous_vehicle/sketch.ino
+++ autonomous_vehicle/sketch.ino
@@ -1,1 +1,10 @@
 #include <engine_control.h>
+
+void setup() {
+  pinMode(engine1, OUTPUT);    
+  pinMode(engine2, OUTPUT);
+  pinMode(input_pin1, OUTPUT);
+  pinMode(input_pin2, OUTPUT);
+  pinMode(input_pin3, OUTPUT);
+  pinMode(input_pin4, OUTPUT);
+}
