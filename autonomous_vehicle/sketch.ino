c158039b9a19c92e2798a633d09c016cd0eaa1dc
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-12-06 19:29:33 +0100

    Update sketch.ino

--- autonomous_vehicle/sketch.ino
+++ autonomous_vehicle/sketch.ino
@@ -1,9 +1,9 @@
 #include <DirectCurrent-Motor-Module.h>
 
 int engine1 = 10;
-int engine2 = 5;
 int input_pin1 = 9;
 int input_pin2 = 8;
+int engine2 = 5;
 int input_pin3 = 7;
 int input_pin4 = 6;
 
@@ -14,4 +14,6 @@ void setup() {
   pinMode(input_pin2, OUTPUT);
   pinMode(input_pin3, OUTPUT);
   pinMode(input_pin4, OUTPUT);
+  DirectCurrent motor1 = { 9, 8, 10 };
+  DirectCurrent motor2 = { 7, 6, 5 };
 }
