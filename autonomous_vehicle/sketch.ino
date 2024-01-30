4de1ea085d883e01e361e70ad760f05e839c73ac
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-01-30 14:00:05 +0100

    Update sketch.ino

--- autonomous_vehicle/sketch.ino
+++ autonomous_vehicle/sketch.ino
@@ -1,7 +1,7 @@
 #include <DirectCurrent-Motor-Module.h>
-#include <Serial-Monitor.h>
 
-SerialMonitor serial;
+DirectCurrent motor1 = { 9, 8, 10 };
+DirectCurrent motor2 = { 7, 6, 5 };
 
 int engine1 = 10;
 int input_pin1 = 9;
@@ -9,6 +9,9 @@ int input_pin2 = 8;
 int engine2 = 5;
 int input_pin3 = 7;
 int input_pin4 = 6;
+int sensor_output;
+int sensor_input;
+long sensor_distance = 0;
 
 void setup() {
   pinMode(engine1, OUTPUT);    
@@ -17,11 +20,21 @@ void setup() {
   pinMode(input_pin2, OUTPUT);
   pinMode(input_pin3, OUTPUT);
   pinMode(input_pin4, OUTPUT);
-  DirectCurrent motor1 = { 9, 8, 10 };
-  DirectCurrent motor2 = { 7, 6, 5 };
-  serial.initialize();
+  pinMode(sensor_output, OUTPUT);
+  pinMode(sensor_input, INPUT);
 }
 
 void loop() {
-  motor1.move(1, 0, 200);
+  motor1.move(motor1, 1, 0, 200);
+  digitalWrite(signal_output, 0);
+  delay(5);
+  digitalWrite(signal_output, 1);
+  delayMicroseconds(10);
+  digitalWrite(signal_output, 0);
+  long time = pulseIn(signal_echo, 1);
+  distance = (time / 2) * 0.03432;
+  delay(500);
+  if (distance < 100) {
+    
+  }
 }
