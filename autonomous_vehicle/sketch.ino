9fda8f7f9f8f576bc66b6fb56a252f50763b651f
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-01-30 15:07:23 +0100

    Update sketch.ino

--- autonomous_vehicle/sketch.ino
+++ autonomous_vehicle/sketch.ino
@@ -1,9 +1,9 @@
-#include <DirectCurrent_Motor_Module.h>
 #include <Servo_Motor_Module.h>
+#include <DirectCurrent_Motor_Module.h>
 
 DirectCurrent motor1 = { 12, 13, 8 };
 DirectCurrent motor2 = { 10, 11, 9 };
-Servo2 servo1 = { 6 };
+Servo2 servo1 = { 3 };
 
 int engine1 = 8;
 int input_pin1 = 12;
@@ -14,10 +14,11 @@ int input_pin4 = 11;
 int sensor_output = 7;
 int sensor_input = 6;
 long sensor_distance = 0;
+int angle1;
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(6);
+  servo1.attach();
   pinMode(engine1, OUTPUT);    
   pinMode(engine2, OUTPUT);
   pinMode(input_pin1, OUTPUT);
@@ -37,7 +38,11 @@ void loop() {
   long time = pulseIn(sensor_input, 1);
   sensor_distance = (time / 2) * 0.03432;
   Serial.println(sensor_distance);
-  delay(500);
+  if (sensor_distance < 20) {
+    motor1.move(1,0,150);
+    motor2.move(0,1,200);
+  }
+}
   if (sensor_distance < 100) {
     motor1.move(1, 0, 100);
   }
